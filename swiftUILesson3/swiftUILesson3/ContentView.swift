c03c1391713dcef99f5cb244472b2249a56c16f4
YauheniDzemidovich <Yauheni.Dzemidovich@gmail.com>
2023-10-05 21:55:41 +0300

    completed

---
+++ swiftUILesson3/swiftUILesson3/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  swiftUILesson3
+//
+//  Created by Yauheni Dzemidovich on 5.10.23.
+//
+
+import SwiftUI
+
+private enum Settings {
+    static let elementSize = 30.0
+    static let duration = 0.25
+    static let imageName = "play.fill"
+}
+
+struct NextTrackButton: View {
+
+    @Binding var isAnimating: Bool
+
+    var body: some View {
+        let startElementSize = isAnimating ? Settings.elementSize : 0.0
+        let endElementSize = isAnimating ? 0.0 : Settings.elementSize
+        Button(action: {
+            self.isAnimating.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + Settings.duration) {
+                isAnimating.toggle()
+            }
+        }) {
+            HStack(spacing: 0) {
+                Image(systemName: Settings.imageName)
+                    .resizable()
+                    .frame(width: startElementSize, height: startElementSize)
+                    .opacity(isAnimating ? 1 : 0.5)
+                Image(systemName: Settings.imageName)
+                    .resizable()
+                    .frame(width: Settings.elementSize, height: Settings.elementSize)
+                Image(systemName: Settings.imageName)
+                    .resizable()
+                    .frame(width: endElementSize, height: endElementSize)
+                    .opacity(isAnimating ? 0.5 : 1)
+            }
+            .animation(isAnimating ? .spring(dampingFraction: 0.5, blendDuration: Settings.duration) : nil, value: isAnimating)
+        }
+    }
+}
+
+struct ContentView: View {
+
+    @State private var isAnimating = false
+    
+    var body: some View {
+        NextTrackButton(isAnimating: $isAnimating)
+            .padding(.all, isAnimating ? Settings.elementSize * 1.5 : Settings.elementSize * 1.2)
+            .background(isAnimating ? .gray.opacity(0.2) : .clear)
+            .clipShape(Circle())
+            .animation(.easeInOut, value: isAnimating)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUILesson3/swiftUILesson3/swiftUILesson3App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUILesson3App.swift
+//  swiftUILesson3
+//
+//  Created by Yauheni Dzemidovich on 5.10.23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUILesson3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
